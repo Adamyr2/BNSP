551e095904ea92036de43dfd920f20ae007bfb2f
Adamyr2 <asdosnf2022>
2023-08-04 14:26:01 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\BooksController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+route::get('/dashboard/book', [BooksController::class, 'index']);
+route::get('/book/pdf', [BooksController::class, 'SaveToPdf'])->name('book.pdf');
